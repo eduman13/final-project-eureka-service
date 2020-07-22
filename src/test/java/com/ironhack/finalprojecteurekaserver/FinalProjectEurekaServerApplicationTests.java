a8c9957f15bb4f2f7512131c8954ab3d0c86605d
eduman13 <eduman13@gmail.com>
2020-07-22 19:13:08 +0200

    Subo el proyecto

---
+++ src/test/java/com/ironhack/finalprojecteurekaserver/FinalProjectEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.finalprojecteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
